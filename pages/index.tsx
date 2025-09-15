88e7ce89f71ca6b3d9789f5d18c0a45439f008a4
NicolasBC <nicolascastela4@gmail.com>
2025-09-15 17:28:59 -0400

    feat: testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,56 +2,43 @@
 import DefaultLayout from "@/layouts/default";
 import { Button } from "@heroui/button";
 import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
-import { Chip } from "@heroui/chip";
 import { Image } from "@heroui/image";
 import { Divider } from "@heroui/react";
-
+import { useEffect, useState } from "react";
 
 export default function IndexPage() {
-  const items = [
-    {
-      urlImage: 'https://app.requestly.io/delay/1000/https://heroui.com/images/fruit-4.jpeg',
-      title: 'Filme Maneiro',
-      category: 'Ação',
-      description: 'Um filme diferente de ação que você vai adorar assistir com seus amigos e familiares.',
-      status: 'Assistido',
-      chipColor: 'success'
-    },
-    {
-      urlImage: 'https://heroui.com/images/hero-card.jpeg',
-      title: 'Filme Maneiro',
-      category: 'Ação',
-      description: 'Um filme diferente de ação que você vai adorar assistir com seus amigos e familiares.',
-      status: 'Pendente',
-      chipColor: 'warning'
-    },
-    {
-      urlImage: 'https://heroui.com/images/fruit-1.jpeg',
-      title: 'Filme Maneiro',
-      category: 'Ação',
-      description: 'Um filme diferente de ação que você vai adorar assistir com seus amigos e familiares.',
-      status: 'Assistindo',
-      chipColor: 'primary'
-    },
-    {
-      urlImage: 'https://heroui.com/images/album-cover.png',
-      title: 'Filme Maneiro',
-      category: 'Ação',
-      description: 'Um filme diferente de ação que você vai adorar assistir com seus amigos e familiares.',
-      status: 'Assistido',
-      chipColor: 'success'
-    },
+  const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    movieList()
+  }, [])
 
-  ]
+  async function movieList() {
+    const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=true&language=pt-BR&page=1&sort_by=popularity.desc';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYjBlMjM1ODBhZWMzMWIxY2VlNDY5ZDNkNmI4ZjE4NyIsIm5iZiI6MTc1NzcwNzYyNC4xMjgsInN1YiI6IjY4YzQ3ZDY4N2ZhNzA5NGU1MGMzYjFjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GLyE88Fli13FhSZAuNTIVZpKtxNV_ip1_xEqnRGGum8'
+      }
+    };
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(json => setMovies(json.results))
+      .catch(err => console.error(err))
+      .finally(() => console.log(movies))
+  }
+
 
 
   return (
     <DefaultLayout>
       <div className="flex flex-wrap gap-4">
-        {items.map((item, index) => (
+
+        {movies.map((item: any, index) => (
           <Card key={index} isFooterBlurred className="w-full sm:w-[calc(50%-0.5rem)] lg:w-[calc(25%-0.75rem)] border-none relative" radius="lg">
-            <CardHeader className="flex gap-3">
+            <CardHeader className="flex gap-3 h-20">
               <Image
                 alt="heroui logo"
                 height={40}
@@ -60,25 +47,25 @@ export default function IndexPage() {
                 width={40}
               />
               <div className="flex flex-col">
-                <p className="text-md">{item.title}</p>
-                <p className="text-small text-default-500">{item.category}</p>
+                <p className="text-md line-clamp-2">{item.title}</p>
+                <p className="text-small text-default-500">{item.release_date}</p>
               </div>
             </CardHeader>
             <Image
-              className="object-cover object-center" alt="Woman listing to music"
-              src={item.urlImage}
+              className="object-cover object-center" alt="Movie poster"
+              src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
               width="100%"
               height={250}
             />
 
 
             <CardBody >
-              <p>{item.description}
+              <p>{item.overview.slice(0, 100)}...
               </p>
             </CardBody>
             <Divider />
             <CardFooter className="flex justify-between" >
-              <Chip variant="solid" color={item.chipColor as any}>{item.status}</Chip>
+              {/* <Chip variant="solid" color={item.chipColor as any}>{item.status}</Chip> */}
               <Button
                 className="text-tiny text-white bg-black /20"
                 color="default"
